71eb6c7354f07abce78d7074546a5abe8028add9
jonataspeixoto <jonatas_micael@hotmail.com>
2023-09-17 23:01:02 -0300

    Formulário: API Spring: Criar Curso (HTTP POST)

--- src/main/java/com/jonatas/crudspring/controller/CourseController.java
+++ src/main/java/com/jonatas/crudspring/controller/CourseController.java
@@ -3,8 +3,14 @@ package com.jonatas.crudspring.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jonatas.crudspring.model.Course;
@@ -22,5 +28,11 @@ public class CourseController {
     @GetMapping
     public List<Course> list(){
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course){
+        return courseRepository.save(course);
     }
 }
